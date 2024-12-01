15fe8ff013e45888819afa2b0e2391db6f616898
Cipo <cipokibona.ck@gmail.com>
2024-12-01 17:49:00 +0200

    changement de base de donnee en sqlite3

--- transparance/transparance/settings.py
+++ transparance/transparance/settings.py
@@ -77,17 +77,22 @@ WSGI_APPLICATION = 'transparance.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/5.1/ref/settings/#databases
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.postgresql_psycopg2',
+#         'NAME': 'transparance_financier',
+#         'USER': 'postgres',
+#         'PASSWORD': 'cipo',
+#         'HOST': '127.0.0.1',
+#         'PORT': '5432',
+#     }
+# }
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.postgresql_psycopg2',
-        'NAME': 'transparance_financier',
-        'USER': 'postgres',
-        'PASSWORD': 'cipo',
-        'HOST': '127.0.0.1',
-        'PORT': '5432',
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
     }
 }
-
 
 # Password validation
 # https://docs.djangoproject.com/en/5.1/ref/settings/#auth-password-validators
